f54e100b095e555a22539c2c1c5e01961e383ec1
domouling <gilberto.domoulinmata@cencosud.cl>
2022-01-27 10:13:22 -0300

    Ajustes em Modelo Subcategoria2

--- models/subcategory2.ts
+++ models/subcategory2.ts
@@ -2,46 +2,6 @@ import { DataTypes, DATE, Model } from "sequelize";
 import db from "../db/connections";
 import { Subcategory } from "./subcategory";
 
-
-/* const Subcategory2 = db.define('subcategoria2', {
-    nombre: {
-        type: DataTypes.STRING
-    },
-    subcategoria_id : {
-        type: DataTypes.INTEGER
-    },
-    jerarquiacat : {
-        type: DataTypes.INTEGER,
-        defaultValue: 0
-    },
-    jerarquiasubcat : {
-        type: DataTypes.INTEGER,
-        defaultValue: 0
-    },
-    jerarquiasubcat2 : {
-        type: DataTypes.INTEGER,
-        defaultValue: 0
-    },
-    status: {
-        type: DataTypes.INTEGER,
-        defaultValue: 1
-    },
-    unidadnegocio: {
-        type: DataTypes.STRING,
-        defaultValue: null
-    },
-    createdAt: {
-        field: 'created_at',
-        type: DataTypes.DATE
-    },
-    updatedAt: {
-        field: 'updated_at',
-        type: DATE
-    }
-}, {
-    freezeTableName: true
-}); */
-
 export class Subcategory2 extends Model {}
 Subcategory2.init({
     nombre: {
