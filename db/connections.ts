a6d9ba4f883162d76f00d423c219b47b89a19d60
Gilberto Domoulin <gilberto.domoulin@gmail.com>
2022-02-14 16:30:01 -0300

    Cambio de conexión

--- db/connections.ts
+++ db/connections.ts
@@ -1,7 +1,7 @@
 import { Sequelize, QueryTypes, Op } from 'sequelize';
 
-const db = new Sequelize('gastos', 'tato', 'Kxqw_10801', {
-    host: 'localhost',
+const db = new Sequelize('gastos', 'bill', 'Kxqw_10801', {
+    host: 'vps-2399293-x.dattaweb.com',
     dialect: 'mysql',
     //logging: false
 });
