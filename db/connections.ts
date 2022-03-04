ddd37fa8fe9b9abd41dd54c4238ba0f95ca27443
Gilberto Domoulin <gilberto.domoulin@gmail.com>
2022-03-04 09:18:19 -0300

    Adec. DB

--- db/connections.ts
+++ db/connections.ts
@@ -1,15 +1,15 @@
 import { Sequelize, QueryTypes, Op } from 'sequelize';
 
-/*const db = new Sequelize('gastos', 'bill', 'Kxqw_10801', {
+const db = new Sequelize('gastos', 'bill', 'Kxqw_10801', {
     host: 'vps-2399293-x.dattaweb.com',
     dialect: 'mysql',
     //logging: false
-});*/
+});
 
-const db = new Sequelize('gastos', 'tato', 'Kxqw_10801', {
+/* const db = new Sequelize('gastos', 'tato', 'Kxqw_10801', {
     host: 'localhost',
     dialect: 'mysql',
     //logging: false
-});
+}); */
 
 export default db;
